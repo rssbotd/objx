d467812fcdb881d669f162a58daf2aef95869609
Bart Thate <bthate@dds.nl>
2024-08-22 23:33:39 +0200

    tune binary

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,4 +1,19 @@
 # This file is placed in the Public Domain.
+# pylint: disable=W0406,W0611
+# ruff: noqa: F401
 
 
 "modules"
+
+
+from . import fnd, log, mod, req, tdo
+
+
+def __dir__():
+    return (
+        'fnd',
+        'log',
+        'mod',
+        'req',
+        'tdo'
+    )
