79a74a5b4f00701df93ae7dc68c53620c1a90f4c
Bart Thate <bart@dunker.home>
2024-08-27 01:45:59 +0200

    add group

---
+++ objx/group.py
@@ -0,0 +1,19 @@
+# This file is placed in the Public Domain.
+
+
+"collections"
+
+
+def union(obj1, obj2):
+    pass    
+
+
+def diff(obj1, obj2):
+    pass
+
+
+def __dir__():
+    return (
+        'diff',
+        'union',
+    )
