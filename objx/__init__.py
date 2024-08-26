267a106628d09acc3945e193e6ad5d437216a9f7
Bart Thate <rssbotd@gmail.com>
2024-08-27 01:58:14 +0200

    update docstring

--- objx/__init__.py
+++ objx/__init__.py
@@ -5,39 +5,26 @@
 
 """objects
 
+OBJX contains all the python3 code to program objects in a functional
+way. It provides a base Object class that has only dunder methods, all
+methods are factored out into functions with the objects as the first
+argument. It is called Object Programming (OP), OOP without the
+oriented.
 
-SYNOPSIS
-
-    >>> from objx import Object, dumps, loads
-    >>> o = Object()
-    >>> o.a = "b"
-    >>> print(loads(dumps(o))
-    {'a': 'b'}
-
-
-INSTALL
-
-    $ pip install objx
+OBJX  allows for easy json save//load to/from disk of objects. It
+provides a "clean namespace" Object class so the namespace is not
+cluttered with method names. This makes storing and reading to/from
+json possible.
 
 
-DESCRIPTION
-
-    OBJX contains all the python3 code to program objects in a functional
-    way. It provides a base Object class that has only dunder methods, all
-    methods are factored out into functions with the objects as the first
-    argument. It is called Object Programming (OP), OOP without the
-    oriented.
-
-    OBJX  allows for easy json save//load to/from disk of objects. It
-    provides a "clean namespace" Object class that only has dunder
-    methods, so the namespace is not cluttered with method names. This
-    makes storing and reading to/from json possible.
+>>> from objx import Object, dumps, loads
+>>> o = Object()
+>>> o.a = "b"
+>>> print(loads(dumps(o))
+{'a': 'b'}
 
 
-COPYRIGHT
-
-    OBJX is Public Domain.
-"""
+OBJX is Public Domain."""
 
 
 from . import decoder, encoder, object
@@ -68,7 +55,6 @@ def __dir__():
         'load',
         'loads',
         'match',
-        'pjoin',
         'read',
         'search',
         'union',
