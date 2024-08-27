900c62fd8292fdca27c3a8d5fb6f38bb20bf46da
Bart Thate <rssbotd@gmail.com>
2024-08-27 02:03:11 +0200

    linted

--- objx/group.py
+++ objx/group.py
@@ -1,19 +1,20 @@
 # This file is placed in the Public Domain.
+# pylint: disable=W0613
 
 
 "collections"
 
 
 def union(obj1, obj2):
-    pass    
+    "concat to object."
 
 
 def diff(obj1, obj2):
-    pass
+    "difference between objects."
 
 
 def __dir__():
     return (
         'diff',
-        'union',
+        'union'
     )
