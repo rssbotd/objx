8dbd6e09abd74e4584961f82410a649034422714
Bart Thate <rssbotd@gmail.com>
2024-09-09 05:35:42 +0200

    add more encoder tests

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -10,7 +10,7 @@
 import unittest
 
 
-from objx.encoder import dumps
+from objx import Object, dumps
 
 
 class TestEncoder(unittest.TestCase):
@@ -24,3 +24,54 @@ class TestEncoder(unittest.TestCase):
         a = "b"
         res = dumps(a)
         self.assertEqual(res, '"b"')
+
+    def test_zero(self):
+        a = 0
+        res = dumps(a)
+        self.assertEqual(res, "0")
+
+    def test_integer(self):
+        a = 1
+        res = dumps(a)
+        self.assertEqual(res, "1")
+
+    def test_zerofloat(self):
+        a = 0.0
+        res = dumps(a)
+        self.assertEqual(res, "0.0")
+
+    def test_float(self):
+        a = 1.1
+        res = dumps(a)
+        self.assertEqual(res, "1.1")
+
+    def test_true(self):
+        a = True
+        res = dumps(a)
+        self.assertEqual(res, "true")
+
+    def test_false(self):
+        a = False
+        res = dumps(a)
+        self.assertEqual(res, "false")
+
+    def test_emptydict(self):
+        a = {}
+        res = dumps(a)
+        self.assertEqual(res, "{}")
+
+    def test_dict(self):
+        a = {"a": "b"}
+        res = dumps(a)
+        self.assertEqual(res, '{"a": "b"}')
+
+    def test_emptyobject(self):
+        o = Object()
+        res = dumps(o)
+        self.assertEqual(res, '{}')
+
+    def test_object(self):
+        o = Object()
+        o.a = "b"
+        res = dumps(o)
+        self.assertEqual(res, '{"a": "b"}')
